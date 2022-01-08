7d38a7dd7629928186032eb391b2dcdefea34869
Rukshan <arkruka@gmail.com>
2022-01-08 15:11:36 +0530

    extension initial commit

---
+++ src/elements/extension.ts
@@ -0,0 +1,59 @@
+import { Element } from "./element";
+
+interface Extension {
+  id: string;
+  _id: Element;
+  url: string;
+  _url: Element;
+  extension: Array<Extension>;
+}
+
+class Extension {
+  constructor(parameters?) {
+    let _: Map<string, any> = new Map();
+
+    if (parameters) {
+      _ = new Map(Object.entries(parameters));
+    }
+
+    _.forEach((value: any, key: string) => {
+      switch (key) {
+        case "id":
+          this.id = key;
+          break;
+
+        case "_id":
+          this._id = new Element(value);
+          break;
+
+        case "url":
+          this.url = value;
+          break;
+
+        case "_url":
+          this._url = new Element(value);
+          break;
+
+        case "extension":
+          if (!Array.isArray(value)) {
+            value = [value];
+          }
+          (value as Array<any>).map((v) => {
+            let temp = new Extension(v);
+            this.extension.push(temp);
+          });
+        default:
+          break;
+      }
+    });
+  }
+  toJSON(): object {
+    return {
+      id: this.id,
+      _id: this._id.toJSON(),
+      extension: this.extension && this.extension.map((e) => e.toJSON()),
+    };
+  }
+}
+
+export { Extension };
