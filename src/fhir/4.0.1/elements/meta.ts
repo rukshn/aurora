b4d57c36268c7b73bb0d0a02844311640c976c01
Rukshan <arkruka@gmail.com>
2022-01-18 11:51:33 +0530

    feat: meta element
    Comleted the meta element and fixed some issues

--- src/fhir/4.0.1/elements/meta.ts
+++ src/fhir/4.0.1/elements/meta.ts
@@ -1,16 +1,106 @@
+import { Coding } from "./coding";
 import { Element } from "./element";
 
 class Meta extends Element {
   private $versionId: string;
+  public get versionId(): string {
+    return this.$versionId;
+  }
+  public set versionId(value: string) {
+    this.$versionId = value;
+    this.$.set("versionId", value);
+    console.log(this.$.get("versionId"));
+  }
+
   private $_versionId: Element;
+  public get _versionId(): Element {
+    return this.$_versionId;
+  }
+  public set _versionId(value: Element) {
+    this.$_versionId = value;
+    this.$.set("_versionId", value.toJSON());
+  }
+
   private $lastUpdated: string;
+  public get lastUpdated(): string {
+    return this.$lastUpdated;
+  }
+  public set lastUpdated(value: string) {
+    this.$lastUpdated = value;
+    this.$.set("lastUpdated", value);
+  }
+
   private $_lastUpdated: Element;
+  public get _lastUpdated(): Element {
+    return this.$_lastUpdated;
+  }
+  public set _lastUpdated(value: Element) {
+    this.$_lastUpdated = value;
+    this.$.set("_lastUpdated", value.toJSON());
+  }
+
   private $source: string;
+  public get source(): string {
+    return this.$source;
+  }
+  public set source(value: string) {
+    this.$source = value;
+    this.$.set("source", value);
+  }
+
   private $_source: Element;
+  public get _source(): Element {
+    return this.$_source;
+  }
+  public set _source(value: Element) {
+    this.$_source = value;
+    this.$.set("_source", value.toJSON());
+  }
+
   private $profile: Array<string>;
+  public get profile(): Array<string> {
+    return this.$profile;
+  }
+  public set profile(value: Array<string>) {
+    this.$profile = value;
+    this.$.set("profile", value);
+  }
+
   private $_profile: Array<Element>;
-  private $security;
-  private $tag;
+  public get _profile(): Array<Element> {
+    return this.$_profile;
+  }
+  public set _profile(value: Array<Element>) {
+    this.$_profile = value;
+    this.$.set(
+      "_profile",
+      value.map((v) => v.toJSON())
+    );
+  }
+
+  private $security: Array<Coding>;
+  public get security(): Array<Coding> {
+    return this.$security;
+  }
+  public set security(value: Array<Coding>) {
+    this.$security = value;
+    this.$.set(
+      "security",
+      value.map((s) => s.toJSON())
+    );
+  }
+
+  private $tag: Array<Coding>;
+  public get tag(): Array<Coding> {
+    return this.$tag;
+  }
+  public set tag(value: Array<Coding>) {
+    this.$tag = value;
+    this.$.set(
+      "tag",
+      value.map((v) => v.toJSON())
+    );
+  }
 
   constructor(parameters?) {
     let _: Map<string, any> = new Map(Object.entries(parameters || {}));
@@ -19,49 +109,42 @@ class Meta extends Element {
     _.forEach((value: any, key: string) => {
       switch (key) {
         case "versionId":
-          this.$versionId = value;
-          this.$.set("versionId", value);
+          this.versionId = value;
           break;
 
         case "_versionId":
-          this.$_versionId = new Element(value);
-          this.$.set("_versionId", this.$_versionId.toJSON());
+          this._versionId = new Element(value);
           break;
 
         case "lastUpdated":
-          this.$lastUpdated = value;
-          this.$.set("lastUpdated", value);
+          this.lastUpdated = value;
           break;
 
         case "_lastUpdated":
-          this.$_lastUpdated = new Element(value);
-          this.$.set("_lastUpdated", this.$_lastUpdated.toJSON());
+          this._lastUpdated = new Element(value);
           break;
 
         case "source":
-          this.$source = value;
-          this.$.set("source", value);
+          this.source = value;
           break;
 
         case "_source":
-          this.$_source = new Element(value);
-          this.$.set("source", this.$_source.toJSON());
+          this._source = new Element(value);
           break;
 
         case "profile":
+          this.$profile = [];
           if (!Array.isArray(value)) {
             value = [value];
           }
-          this.$profile = [];
-          this.$profile = value as Array<string>;
-          this.$.set("profile", value);
+          this.profile = value;
           break;
 
         case "_profile":
+          this.$_profile = [];
           if (!Array.isArray(value)) {
             value = [value];
           }
-          this.$_profile = [];
           (value as Array<any>).map((v) => {
             let temp = new Element(v);
             this.$_profile.push(temp);
@@ -73,108 +156,37 @@ class Meta extends Element {
           );
 
         case "security":
+          this.$security = [];
+          if (!Array.isArray(value)) {
+            value = [value];
+          }
+          (value as Array<any>).map((v) => {
+            let temp = new Coding(v);
+            this.$security.push(temp);
+          });
+          this.$.set(
+            "security",
+            this.$security.map((s) => s.toJSON())
+          );
           break;
         case "tag":
+          this.$tag = [];
+          if (!Array.isArray(value)) {
+            value = [value];
+          }
+          (value as Array<any>).map((v) => {
+            let temp = new Coding(v);
+            this.$tag.push(temp);
+          });
+          this.$.set(
+            "tag",
+            this.$tag.map((t) => t.toJSON())
+          );
           break;
         default:
-          this.$.set(key, value);
           break;
       }
     });
-  }
-
-  public set versionId(v: string) {
-    this.$versionId = v;
-    this.$.set("versionId", v);
-  }
-
-  public get versionId(): string {
-    return this.$versionId;
-  }
-
-  public set _versionId(v: Element) {
-    this.$_versionId = v;
-    this.$.set("_versionId", v.toJSON());
-  }
-
-  public get _versionId(): Element {
-    return this.$_versionId;
-  }
-
-  public set lastUpdated(v: string) {
-    this.$lastUpdated = v;
-    this.$.set("lastUpdated", v);
-  }
-
-  public get lastUpdated(): string {
-    return this.$lastUpdated;
-  }
-
-  public set _lastUpdated(v: Element) {
-    this.$_lastUpdated = v;
-    this.$.set("_lastUpdated", v.toJSON());
-  }
-
-  public get _lastUpdated(): Element {
-    return this.$_lastUpdated;
-  }
-
-  public set source(v: string) {
-    this.$source = v;
-    this.$.set("source", v);
-  }
-
-  public get source(): string {
-    return this.$source;
-  }
-
-  public set _source(v: Element) {
-    this.$_source = v;
-    this.$.set("_source", v.toJSON());
-  }
-
-  public get _source(): Element {
-    return this.$_source;
-  }
-
-  public set profile(v: Array<string>) {
-    this.$profile = v;
-    if (!Array.isArray(v)) {
-      v = [v];
-    }
-    this.$.set("profile", v);
-  }
-
-  public get profile(): Array<string> {
-    return this.$profile;
-  }
-
-  public set _profile(v: Array<Element>) {
-    this._profile = v;
-    this.$.set(
-      "_profile",
-      v.map((v) => v.toJSON())
-    );
-  }
-
-  public get _profile(): Array<Element> {
-    return this.$_profile;
-  }
-
-  public set security(v: string) {
-    this.security = v;
-  }
-
-  public get security(): string {
-    return this.$security;
-  }
-
-  public set tag(v: string) {
-    this.$tag = v;
-  }
-
-  public get tag(): string {
-    return this.$tag;
   }
 }
 
