654f1d76c8df46e3de4dc5d2ef0cac14f4ee9e0d
Rukshan <arkruka@gmail.com>
2022-01-18 12:10:40 +0530

    docs: Added code comments

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ const app = express();
 const cors = require("cors");
 const patientRoutes = require("./routes/patientRoutes");
 
+// The default port for the FHIR server is port 3000
 const port = 3000;
-
+// Enable cors for express
 app.use(cors());
+// Enable accepting json content
 app.use(express.json());
+// Get the patient routes
 app.use("/Patient", patientRoutes);
-
+// Default endpoint
 app.get("/", (req, res) => {
   res.json(`FHIR server running at port ${port}`);
 });
-
+// Listen to the given port
 app.listen(port, () => {
   console.log(`🔥 FHIR server listening at http://127.0.0.1:${port}`);
 });
